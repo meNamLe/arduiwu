b81605368628d01d0440931cf820312045a5dd2f
NamLe <vnamle1@gmail.com>
2019-03-24 09:51:43 -0400

    hackathon presentation

--- something.c
+++ something.c
@@ -1,3 +1,7 @@
+/*
+  Arduino Code not C. 
+  Createed By Nam Le on 3-24-19
+*/
 #include <Servo.h> 
 
 int servo1Pin = 3;
@@ -19,6 +23,12 @@ int trVal = 0;
 int tlVal = 0;
 int brVal = 0;
 int blVal = 0;
+
+int led11 = 11;
+
+//total of LDR values
+int total;
+
 // Sides of crosshair; The sum of two LDRs from one side
 int top = 0;
 int bot = 0;
@@ -36,16 +46,18 @@ void setup() {
 }
 
 void loop() {
-  delay(750);
+  delay(20);
   trVal = analogRead(A0);
   tlVal = analogRead(A1);
   brVal = analogRead(A2);
   blVal = analogRead(A3);
+  /*
   Serial.println(trVal);
   Serial.println(tlVal);
   Serial.println(brVal);
-  Serial.println(blVal); 
-  
+  Serial.println(blVal);
+  */ 
+  /*
   servo1Val = Servo1.read();
   servo2Val = Servo2.read();
   
@@ -57,35 +69,42 @@ void loop() {
   
   // threshold to cause change up and down is 100
   if(top > bot && top - bot > 100) {
-      Serial.println("top");
-      Serial.println(servo1Val);  
-      if(servo1Val < 176) {
-          Servo1.write(servo1Val + 10);          
+      //Serial.println("top");
+      //Serial.println(servo1Val);  
+      if(servo1Val < 174) {
+          Servo1.write(servo1Val + 1);          
       }
   } else if(bot - top > 100) {
-      Serial.println("bottom");
-      Serial.println(servo1Val);
-      if(servo1Val > 4) {
-          Servo1.write(servo1Val - 10);          
+      //Serial.println("bottom");
+      //Serial.println(servo1Val);
+      if(servo1Val > 4 ) {
+          Servo1.write(servo1Val - 1);          
       }
   }
   
   if(left > right && left - right > 100) {
-    Serial.println("left");
-    Serial.println(servo2Val);
-    if(servo2Val > 4) {
-      Servo2.write(servo2Val - 10);
+    //Serial.println("left");
+    //Serial.println(servo2Val);
+    if(servo2Val < 176) {
+      Servo2.write(servo2Val + 1);
     }
   } else if(right - left > 100) {
-    Serial.println("right");
-    Serial.println(servo2Val);
-    if(servo2Val < 176) {
-      Servo2.write(servo2Val + 10);
+    //Serial.println("right");
+    //Serial.println(servo2Val);
+    if(servo2Val > 4) {
+      Servo2.write(servo2Val - 1);
     }
   }
+  */
+
+  total = trVal + tlVal + brVal + blVal;
+  Serial.println(total);
+  total = constrain(total,50,250);
+  total = map(total,50,250,0,255);
+  analogWrite(led11,total);
   
 
-  Serial.println("--------");
+  Serial.println(" ");
   
   
 
