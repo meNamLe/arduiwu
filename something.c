464689d5d9b0d14c31b06b1302da730f5832d270
Nam Le <vnamle1@gmail.com>
2019-07-27 19:23:10 -0400

    final

--- something.c
+++ something.c
@@ -51,61 +51,11 @@ void loop() {
   tlVal = analogRead(A1);
   brVal = analogRead(A2);
   blVal = analogRead(A3);
-  /*
-  Serial.println(trVal);
-  Serial.println(tlVal);
-  Serial.println(brVal);
-  Serial.println(blVal);
-  */ 
-  /*
-  servo1Val = Servo1.read();
-  servo2Val = Servo2.read();
-  
-  top = trVal + tlVal;
-  bot = brVal + blVal;
-  left = tlVal + blVal;
-  right = trVal + brVal;
-
-  
-  // threshold to cause change up and down is 100
-  if(top > bot && top - bot > 100) {
-      //Serial.println("top");
-      //Serial.println(servo1Val);  
-      if(servo1Val < 174) {
-          Servo1.write(servo1Val + 1);          
-      }
-  } else if(bot - top > 100) {
-      //Serial.println("bottom");
-      //Serial.println(servo1Val);
-      if(servo1Val > 4 ) {
-          Servo1.write(servo1Val - 1);          
-      }
-  }
-  
-  if(left > right && left - right > 100) {
-    //Serial.println("left");
-    //Serial.println(servo2Val);
-    if(servo2Val < 176) {
-      Servo2.write(servo2Val + 1);
-    }
-  } else if(right - left > 100) {
-    //Serial.println("right");
-    //Serial.println(servo2Val);
-    if(servo2Val > 4) {
-      Servo2.write(servo2Val - 1);
-    }
-  }
-  */
 
   total = trVal + tlVal + brVal + blVal;
   Serial.println(total);
   total = constrain(total,50,250);
   total = map(total,50,250,0,255);
-  analogWrite(led11,total);
-  
-
-  Serial.println(" ");
-  
-  
+  analogWrite(led11,total);  
 
 }
